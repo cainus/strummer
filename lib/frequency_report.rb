f599bf1c5d66e4393b0143f1a59e0ff197545465
Seth Edwards <sedwards@exacttarget.com>
2011-12-03 18:51:30 -0800

    temporary nonsense

--- lib/frequency_report.rb
+++ lib/frequency_report.rb
@@ -6,7 +6,14 @@ class FrequencyReport
   def test
 
     @db = Mongo::Connection.new("127.0.0.1", "27017").db('cotweet_dev')
-    @db.collection("development_log").find()
+    cursor = @db.collection("development_log").find({"$or" => [{'messages.fatal' => {"$exists" => true}}, {'messages.error' => {"$exists" => true}}]})
+    cursor.each do |doc|
+      if doc["messages"]["error"]
+        doc["messages"]["error"].each do |message|
+          @db.collection("test").insert({:message => message, :last_occurance => doc["request_time"]})
+        end
+      end
+    end
   end
 
 
